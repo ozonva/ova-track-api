a0e9bdae6066e44ca837379d1b3764c23ee18fd7
dmskvn <denismoskvin4@gmail.com>
2021-08-29 12:36:16 +0300

    task 6

--- Makefile
+++ Makefile
@@ -1,3 +1,28 @@
+LOCAL_BIN:=$(CURDIR)/bin
+
+.PHONY: deps
+deps:
+	go get -u github.com/onsi/ginkgo
+	go get -u github.com/onsi/gomega
+	go get -u github.com/golang/mock
+	go get -u github.com/rs/zerolog/log
+	go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+	go get -u github.com/golang/protobuf/proto
+	go get -u github.com/golang/protobuf/protoc-gen-go
+	go get -u google.golang.org/grpc
+	go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
+	go get -u github.com/rs/zerolog/log
+	go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
+	go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+
+.PHONY: generate
+make generate:
+	protoc --proto_path=. -I vendor.protogen \
+	--go_out=pkg/api --go_opt=paths=import \
+	--go-grpc_out=pkg/api --go-grpc_opt=paths=import \
+	api/api.proto
+
+
 build:
 	go build -o bin/main cmd/ova-track-api/main.go
 
@@ -6,3 +31,4 @@ run:
 
 test:
 	go test  ./...
+
--- cmd/ova-track-api/main.go
+++ cmd/ova-track-api/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"github.com/ozonva/ova-track-api/internal/utils"
-	"time"
-
-	//	. "github.com/onsi/ginkgo"
-	//	. "github.com/onsi/gomega"
+	track_server "github.com/ozonva/ova-track-api/pkg/api/github.com/ova-conference-api/pkg/ova-conference-api"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"net"
+	"strconv"
 )
-import "github.com/ozonva/ova-track-api/internal/flusher"
-import "github.com/ozonva/ova-track-api/internal/repo"
-import "github.com/ozonva/ova-track-api/internal/saver"
 
 var id = uint64(0)
 func GenerateTrack (n int,  name string)  utils.Track {
@@ -29,23 +27,26 @@ func GenerateTrack (n int,  name string)  utils.Track {
 //path := os.Args[1]
 //utils.InitLibraryFromFile(path)
 
+
 func main() {
 
-	rep := repo.PrintRepo{}
-	fl := flusher.NewChunkFlusher(5, rep)
-	svr := saver.NewBufferSaver(fl)
-	bs := saver.NewTimelapseBufferedSaver(svr)
-	go bs.Init(500)
+	port := ":"+strconv.Itoa(8080)
+	listen, err := net.Listen("tcp", port)
+	log.Printf("TCP at %v is starting up ...", port)
 
-	for i := 0; i < 10000; i++{
-		var tracks = []utils.Track{
-			GenerateTrack(i+1,"a"),
-			GenerateTrack (i+1, "b"),
-			GenerateTrack (i+1, "c"),
-			GenerateTrack (i+1, "d"),
-			GenerateTrack (i+1, "e"),
-		}
-		bs.Save(tracks)
-		time.Sleep(time.Second)
+	if err != nil {
+		log.Printf("Failed to listen server %v", err)
 	}
+
+	log.Printf("TCP at %v started successfully", port)
+
+
+	grpcService := grpc.NewServer()
+	track_server.RegisterTrackServer(grpcService, track_server.UnimplementedTrackServer{})
+
+	log.Print("Starting track service")
+	if err := grpcService.Serve(listen); err != nil {
+		log.Printf("failed to serve: %v", err)
+	}
+	log.Printf ("Track service started successfully")
 }
